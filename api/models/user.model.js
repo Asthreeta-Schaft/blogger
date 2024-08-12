d6565aa570c155431dcfb763f3d59f2702719dfc
Asthreeta Schaft <asthreetaschaft@gmail.com>
2024-08-12 11:52:14 +0530

    add admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,12 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: 'https://imgs.search.brave.com/SL3JV4LFCh9nhP0Py8sxF9OCNG-O1Fh2AIJYwk3l7xI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvamVycnktbW91/c2UtcGljdHVyZXMt/cjIzcWV3aWpuM2Fn/cnc5OC5qcGc',
     },
-    },{timestamps: true}
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    },
+    {timestamps: true}
 );
 
 const User = mongoose.model('User', userSchema);
