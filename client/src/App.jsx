fd82fb1acb7d64228a87a52a6a4d5d6b14d7198b
Asthreeta Schaft <asthreetaschaft@gmail.com>
2024-08-13 17:16:46 +0530

    complete about and projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import { About }from './pages/About';
+import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
